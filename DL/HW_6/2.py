defb3f8b75587295be49f9488e60d088bd20826a
Stanislav <smilyanskiy.s@ajax.systems>
2021-10-04 22:44:44 +0300

    initial

---
+++ DL/HW_6/2.py
@@ -0,0 +1,18 @@
+"""Вам подаются на вход два вектора a и b в трехмерном пространстве.
+Реализуйте их скалярное произведение с помощью numpy и без. """
+
+import numpy as np
+
+def no_numpy_scalar(v1, v2):
+    #param v1, v2: lists of 3 ints
+    #YOUR CODE: please do not use numpy
+
+    result = #YOUR CODE
+    return result
+
+def numpy_scalar (v1, v2):
+    #param v1, v2: np.arrays[3]
+    #YOUR CODE
+
+    result = #YOUR CODE
+    return result
